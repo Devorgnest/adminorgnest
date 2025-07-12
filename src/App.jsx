81b2b47bc28f7a578e3b8e943785565eae5dd6ca
Devorgnest <Devorgnest@gmail.com>
2025-07-12 12:33:02 -0600

    Add files via upload
    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminReview from "./components/Admin/AdminReview";
+
+function App() {
+  return (
+  <div>
+  <AdminReview/>
+  </div>
+  );
+}
+
+export default App;
